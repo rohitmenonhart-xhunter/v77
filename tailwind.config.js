fccee4ae35adb2baa577541cd5e05902bf18a9e5
Rohit menon <140264361+rohitmenonhart-xhunter@users.noreply.github.com>
2024-12-30 03:33:46 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -80,5 +80,5 @@ export default {
       },
     },
   },
-  plugins: [await import('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate')],
 };
